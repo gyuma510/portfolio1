93ec2881d57ee3c019dfd0ba2662a624c4f84a22
yumagoto <yu-a.t.0z7@docomo.ne.jp>
2023-02-17 11:07:52 +0900

    route.rb再度コメントイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  #devise_for :users, controllers: {
-    #registrations: 'users/registrations',
-    #passwords: 'users/passwords',
-    #confirmations: 'users/confirmations'
-  #}
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations'
+  }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
     get '/users', to: 'devise/registrations#new'
