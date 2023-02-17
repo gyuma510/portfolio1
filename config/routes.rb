da136aca0dd1ca4a3193b6beca3ed974738f8f0b
yumagoto <yu-a.t.0z7@docomo.ne.jp>
2023-02-17 10:54:46 +0900

    route.rbコメントイン

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
     passwords: 'users/passwords',
     confirmations: 'users/confirmations'
   }
-  #devise_scope :user do
-    #post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
-    #get '/users', to: 'devise/registrations#new'
-    #get 'users/password', to: 'users/passwords#new'
-  #end
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    get '/users', to: 'devise/registrations#new'
+    get 'users/password', to: 'users/passwords#new'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :remember_you do
     resources :records do
