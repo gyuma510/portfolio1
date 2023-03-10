0e578253c8baffd164eb5552fc097f9e76a0ddd4
yumagoto <yu-a.t.0z7@docomo.ne.jp>
2023-03-10 17:07:01 +0900

    ゲストユーザーお問い合わせ機能制限

--- app/controllers/remember_you/contacts_controller.rb
+++ app/controllers/remember_you/contacts_controller.rb
@@ -1,5 +1,6 @@
 class RememberYou::ContactsController < ApplicationController
   before_action :authenticate_user!
+  before_action :guest_check, only: [:create]
   def new
     @contact = Contact.new
   end
@@ -12,6 +13,13 @@ class RememberYou::ContactsController < ApplicationController
     else
       flash.now[:failer] = "送信できませんでした"
       render :new
+    end
+  end
+
+  def guest_check
+    if current_user == User.find_by(email: "guests@example.com")
+      flash[:notice] = "お問い合わせを送信するには、会員登録が必要です。"
+      redirect_to remember_you_records_path
     end
   end
 
